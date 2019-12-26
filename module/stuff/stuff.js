780a0bfebca53563d054072e077e978af7196fc3
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-12-26 14:27:54 +0700

    module export object

--- module/stuff/stuff.js
+++ module/stuff/stuff.js
@@ -1,3 +1,30 @@
+// let counter = function(arr) {
+//   return 'there are ' + arr.length + ' elements in this array';
+// };
+
+// let adder = function(a, b) {
+//   return `the sum of the 2 number is ${a + b}`;
+// };
+
+// var pi = 3.142;
+
+// module.exports.counter = counter;
+// module.exports.adder = adder;
+// module.exports.pi = pi;
+
+
+
+
+// module.exports.counter = function(arr) {
+//   return 'there are ' + arr.length + ' elements in this array';
+// };
+
+// module.exports.adder = function(a, b) {
+//   return `the sum of the 2 number is ${a + b}`;
+// };
+
+// module.exports.pi = 3.142;
+
 let counter = function(arr) {
   return 'there are ' + arr.length + ' elements in this array';
 };
@@ -6,8 +33,10 @@ let adder = function(a, b) {
   return `the sum of the 2 number is ${a + b}`;
 };
 
-var pi = 3.142;
+let pi = 3.142;
 
-module.exports.counter = counter;
-module.exports.adder = adder;
-module.exports.pi = pi;
+module.exports = {
+  counter: counter,
+  adder: adder,
+  pi: pi
+};
