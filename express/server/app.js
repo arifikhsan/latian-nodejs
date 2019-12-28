7a2d5060f05e924e74e41859d9e650e292a7eeef
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-12-28 17:49:19 +0700

    foreach

--- express/server/app.js
+++ express/server/app.js
@@ -16,7 +16,11 @@ app.get('/api', function(request, response) {
 });
 
 app.get('/profile/:name', function(request, response) {
-  let data = { age: 29, job: 'ninja' };
+  let data = {
+    age: 29,
+    job: 'ninja',
+    hobbies: ['eating', 'fighting', 'fishing']
+  };
   response.render('profile', { person: request.params.name, data: data });
 });
 
