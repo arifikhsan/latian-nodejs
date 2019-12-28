2f03bfc441c28d1890d8e71a1be854e7f3c1b526
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-12-28 17:30:14 +0700

    express route params

--- express/server/app.js
+++ express/server/app.js
@@ -1,13 +1,22 @@
-let express = require('express')
+let express = require('express');
 
 let app = express();
 app.get('/', function(request, response) {
-  response.send('this is the homepage')
-})
+  response.send('this is the homepage');
+});
+
 app.get('/contact', function(request, response) {
   response.send('this is the contact page');
 });
+
 app.get('/api', function(request, response) {
-  response.send({message: 'Hello world'});
+  response.send({ message: 'Hello world' });
 });
+
+app.get('/profile/:name', function(request, response) {
+  response.send(
+    'You requested to see ap profile with name of ' + request.params.name
+  );
+});
+
 app.listen(3000);
